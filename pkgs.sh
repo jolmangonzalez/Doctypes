55c1edd07183542e92c1e29845976058efc4c4b1
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2023-07-14 18:19:23 -0500

    Agregue archivos

---
+++ pkgs.sh
@@ -0,0 +1,190 @@
+#system
+acpi
+aic94xx-firmware # AUR
+alsa-firmware
+alsa-tools
+amb-plugins
+
+# Development
+adminer 
+
+# Utilities
+alacritty
+arandr
+
+# Filesystems tools
+android-file-transfer
+android-tools
+
+# Themes
+arc-gtk-theme
+arch-rebuild-order
+arch-repro-status
+arch-signoff
+arch-wiki-lite
+ark 23.04.3-1
+aspell-es 1.11-9
+autorandr 1.14-1
+base 3-1
+base-devel 1-1
+bat 0.23.0-3
+batify 1.0-3
+batsignal 1.6.4-1
+blackarch-keyring 20180925-5
+blueman 2.3.5-2
+bluez-cups 5.68-1
+bluez-utils 5.68-1
+brave-bin 1:1.52.129-1
+bridge-utils 1.7.1-1
+brightnessctl 0.5.1-2
+broadcom-wl-dkms 6.30.223.271-36
+bspwm 0.9.10-3
+btop 1.2.13-1
+cadence 0.9.2-2
+calc 2.14.1.6-1
+caps 0.9.26-2
+cbatticon 1.6.13-1
+checkupdates+aur 1-3
+clinfo 3.0.21.02.21-1
+cmt 1.18-3
+cpio 2.14-1
+cups-pdf 3.0.1-7
+curlftpfs 0.9.2-8
+deepin-icon-theme 2021.11.24-1
+dpf-plugins 1.7-1
+ecryptfs-utils 111-7
+exfat-utils 1.4.0-2
+extra-cmake-modules 5.108.0-1
+ffmpeg-compat-57 3.4.11-1
+ffmpeg0.10 0.10.16-5
+fil-plugins 0.3.0-7
+filelight 23.04.3-1
+firefox-i18n-es-mx 115.0.2-1
+flameshot 12.1.0-1
+font-manager 0.8.8-1
+fscrypt 0.3.4-1
+fzf 0.42.0-1
+gnome-font-viewer 44.0-1
+gparted 1.5.0-1
+grub 2:2.12rc1-1
+gst-plugins-espeak 0.5.0-4
+gufw 22.04-2
+gwenview 23.04.3-1
+htop 3.2.2-1
+hwdetect 2023.07.02-1
+ibus-m17n 1.4.19-1
+imagewriter 1.10.1432200249.1d253d9.2.16-1
+inkscape 1.2.2-9
+intel-ucode 20230613-1
+iucode-tool 2.3.1-4
+jack_mixer 17-3
+kcalc 23.04.3-1
+kcolorchooser 23.04.3-1
+krita 5.1.5-10
+lbzip2 2.5-5
+lhasa 0.3.1-3
+lib32-alsa-oss 1.1.8-4
+libcddb 1.3.2-7
+libreoffice-fresh-es 7.5.4-1
+libva-utils 2.18.2-1
+libvdpau-va-gl 0.4.2-3
+lightdm-gtk-greeter-settings 1.2.2-8
+linux-zen 6.4.3.zen1-1
+lsb-release 2.0.r48.3cf5103-1
+lsd 0.23.1-2
+lxappearance 0.6.3-4
+lxrandr 0.3.2-3
+lynis 3.0.8-1
+lzip 1.23-2
+mcp-plugins 0.4.0-6
+mesa-demos 9.0.0-2
+meterbridge 0.9.3-3
+mpd 0.23.13-2
+msmtp 1.8.22-2
+mtpfs 1.1-4
+mypy 1.3.0-1
+nano 7.2-1
+neofetch 7.1.0-2
+nerd-fonts-meta 20230501-2
+netctl 1.28-2
+network-manager-sstp 1.3.1-2
+networkmanager-dmenu-git r177.265230c-1
+networkmanager-l2tp 1.20.8-1
+networkmanager-openconnect 1.2.10-1
+networkmanager-openvpn 1.10.2-3
+networkmanager-pptp 1.2.12-1
+networkmanager-strongswan 1.6.0-1
+networkmanager-vpnc 1.2.8-3
+nfs-utils 2.6.3-1
+nmap 7.94-1
+numlockx 1.2-5
+okular 23.04.3-1
+opencl-headers 2:2023.04.17-2
+pamac-aur 11.0.1-2
+pamixer 1.6-2
+parcellite 1.2.2-1
+pasystray 0.8.2-1
+php-apache 8.2.8-1
+php-cgi 8.2.8-1
+php-gd 8.2.8-1
+php-mcrypt 8.0.0-1
+php-pgsql 8.2.8-1
+picom 10.2-1
+pkgfile 21-2
+pkgstats 3.2.10-1
+polkit-kde-agent 5.27.6-1
+polybar 3.6.3-3
+pulseaudio-equalizer-ladspa 3.0.2-6
+pulseeffects-legacy 4.8.7-1
+pyside2 5.15.10-1
+python-colorama 0.4.6-2
+pywal-16-colors 3.4.0.r0.g6bf6234-1
+qbittorrent 4.5.4-1
+qjackctl 0.9.11-1
+qtile 0.22.1-4
+ranger 1.9.3-9
+reflector 2023-1
+ruby-sass 3.7.4-3
+sof-firmware 2.2.6-1
+sshguard 2.4.3-1
+sway 1:1.8.1-1
+systemd-swap 4.4.0-2
+tap-plugins 1.0.1-2
+terminus-font 4.49.1-5
+tlpui 1:1.5.0.7-1
+ttf-cascadia-code 2111.01-1
+ttf-hack 3.003-6
+ttf-inconsolata 1:3.000-3
+ttf-meslo 1.2.1-3
+ttf-meslo-nerd-font-powerlevel10k 2.3.3-1
+ttf-octicons 19.1.0-1
+udiskie 2.4.2-2
+unarj 2.65-1
+unzip 6.0-19
+upd72020x-fw 1.0.0-1
+vdpauinfo 1.5-1
+visual-studio-code-bin 1.80.0-1
+vlc 3.0.18-12
+vnstat 2.10-1
+volumeicon 0.5.1-5
+wd719x-firmware 1-7
+wget 1.21.4-1
+wireless_tools 30.pre9-3
+x42-plugins 20230706-1
+xdg-user-dirs-gtk 0.11-2
+xdman 1:7.2.11-2
+xf86-input-evdev 2.10.6-3
+xf86-input-synaptics 1.9.2-1
+xf86-video-intel 1:2.99.917+923+gb74b67f0-1
+xf86-video-vesa 2.6.0-1
+xfce4-settings 4.18.3-1
+xine-lib 1.2.13-4
+xkeycaps 2.47-3
+xorg-xbacklight 1.2.3-3
+xorg-xinit 1.4.2-1
+xorg-xinput 1.6.4-1
+xorg-xsetroot 1.1.3-1
+yay 12.1.0-1
+zip 3.0-10
+zram-generator 1.1.2-1
+zynaddsubfx 3.0.6-4
